158c37c9e1e6f1aee769afb05db6308b226e3b26
Akın UYAR <akinuyar@yandex.com>
2021-06-03 01:42:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+
+const BOT_API       = process.env.BOT_API || '';
+const PORT          = process.env.PORT || 3000;
+const URL           = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+const { Telegraf } = require('telegraf')
+const bot       = new Telegraf(BOT_API);
+
+const config = require('./config');
+
+// Bota start verdiğinizde atılan ilk mesaj
+bot.start((ctx) => {
+    return ctx.reply("Hey");
+});
+
+
+bot.hears(/selam/ig, async (ctx, next) => {
+    await ctx.telegram.sendPhoto(ctx.chat.id,
+        'https://www.ajanskirim.com/wp-content/uploads/2019/03/1525995635Merhaba.jpg',
+        { caption:  `<b>${ctx.from.first_name}</b>`,  parse_mode: 'HTML' })
+    return next();
+});
+
+
+bot.command('botsohbet', async (ctx, next) => {
+    
+    await bot.telegram.sendDocument(ctx.chat.id, {
+        source: './dosyalar/botsohbet.mp4'
+    }, {
+        filename: 'botsohbet.mp4',
+        caption: 'https://t.me/botsohbet'
+    })
+    return next()
+    
+})
+
+
+bot.command('komut', async (ctx, next) => {
+    await ctx.telegram.sendMessage(ctx.chat.id, `<b>${ctx.from.first_name}</b>`, { parse_mode: 'HTML' })
+    return next();
+});
+
+
+bot.use(
+    require('./handlers/middlewares'),
+    require('./plugin')
+);
+
+// Kodlarda hata çıkarsa bunun sayesinde çalışmaya devam eder.
+bot.catch((err) => {
+    console.log('Error: ', err)
+})
+
+// Botun kullanıcı adını alan bir kod.
+bot.telegram.getMe().then(botInfo => {
+    bot.options.username = botInfo.username
+    console.log(`Bot Başlatıldı! => ${bot.options.username}`)
+})
+
+// Heroku sitesinde botunuzun kullanıcı adı gözükür -> deneyselbot.herokuapp.com
+const cb = function(req, res) {
+    res.end(`${bot.options.username}`)
+}
+
+// Botun webhook ile çalışmasını sağlar.
+bot.launch({
+    webhook: {
+        domain: `${URL}`,
+        port: `${PORT}`,
+        cb
+    }
+})
+
+// Bu botumuzu nazikçe durdurmayı etkinleştirir.
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
